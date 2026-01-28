17a7bf2fd69e9883803053f82d4012cc8aabe451
target <aminesalem2223@gmail.com>
2026-01-28 22:13:58 +0100

    add modification in the base.py

--- app/db/base.py
+++ app/db/base.py
@@ -1,9 +1,1 @@
 from app.db.session import Base # noqa to ignore unused import warnings
-
-from app.models.user import User #noqa
-from app.models.category import Category #noqa
-from app.models.listing import Listing #noqa
-from app.models.listing_image import ListingImage #noqa
-from app.models.favorite import Favorite #noqa
-from app.models.conversation import Conversation #noqa
-from app.models.message import Message #noqa
